159b1ec7ed1af8648cc9bc7e85095b1a7670d0b0
Ian Sinnott <ian@iansinnott.com>
2023-09-12 12:02:41 +0800

    implement int tokenization

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,15 @@ import { Tokenizer } from ".";
 
 describe("tokenize", () => {
   test("integers", () => {
-    const t = new Tokenizer({ source: "8" });
-    expect(t.next()).toEqual(["NUMBER", 8]);
+    const tt: [string, any][] = [
+      ["8", ["NUMBER", 8]],
+      ["888", ["NUMBER", 888]],
+      ["23", ["NUMBER", 23]],
+    ];
+
+    for (const [i, o] of tt) {
+      const t = new Tokenizer({ source: i });
+      expect(t.next()).toEqual(o);
+    }
   });
 });
